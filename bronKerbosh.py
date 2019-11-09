5655198068dcb099c6cf4f01e4fc8450b1ef87e4
Felipe <narulee@hotmail.com>
2019-11-09 15:57:11 -0300

    p.remove()

--- bronKerbosh.py
+++ bronKerbosh.py
@@ -3,27 +3,27 @@ from igraph import*
 def bronKerbosh(cliqueMax, g, r, p, x):
 
     if len(p) == 0 and len(x) == 0:
-        print("achei clique maxima")
+        # if len(r) == 8:
+        # print("achei clique maxima")
         cliqueMax.append(r)
         return
 
-    # print(p)
-    # print(x)
-    print("P u X: {}".format(list(set().union(p, x))))
+    # print("P u X: {}".format(list(set().union(p, x))))
     u = escolheVertice(g, list(set().union(p, x)))
 
     # u = g.maxdegree(list(set().union(p, x)))
-    print("escolhido: ", u)
+    # print("escolhido: ", u)
 
-    print("P\N({}) = {}".format(u, str(list(set(p) - set(g.neighbors(u))))))
+    # print("P\N({}) = {}".format(u, str(list(set(p) - set(g.neighbors(u))))))
     for v in list(set(p) - set(g.neighbors(u))):
-        # r.append(v)
-        print("r = {} p = {} x = {}".format(r + [v], list(set(p) & set(g.neighbors(v))), list(set(x) & set(g.neighbors(v)))))
+        # print("v: {}".format(v))
+        # print("r = {} p = {} x = {}".format(r + [v], list(set(p) & set(g.neighbors(v))), list(set(x) & set(g.neighbors(v)))))
         bronKerbosh(cliqueMax, g, r + [v], list(set(p) & set(g.neighbors(v))), list(set(x) & set(g.neighbors(v))))
-        p = list(set(p) - set(g.neighbors(v)))
-        x = x + [v]
-        print("p = {} x = {}".format(p, x))
-        print("fimbk")
+        # print("p = {} - {}".format(list(set(p)), list(set(g.neighbors(v)))))
+        p.remove(v)
+        x.append(v)
+        # print("p = {} x = {}".format(p, x))
+        # print("fimbk")
 
 def escolheVertice(g, vertices):
     grau = []
