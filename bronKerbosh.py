09baf62e4d90126e7d97335815501fb4bd984bfd
Felipe <narulee@hotmail.com>
2019-11-29 13:47:57 -0300

    finalizado.

--- bronKerbosh.py
+++ bronKerbosh.py
@@ -1,29 +1,18 @@
 from igraph import*
 
-def bronKerbosh(cliqueMax, g, r, p, x):
+def bronKerbosh(n, cliqueMax, g, r, p, x):
 
     if len(p) == 0 and len(x) == 0:
-        if len(r) == 8:
-        # print("achei clique maxima")
+        if len(r) == n:
             cliqueMax.append(r)
         return
 
-    # print("P u X: {}".format(list(set().union(p, x))))
     u = escolheVertice(g, list(set().union(p, x)))
 
-    # u = g.maxdegree(list(set().union(p, x)))
-    # print("escolhido: ", u)
-
-    # print("P\N({}) = {}".format(u, str(list(set(p) - set(g.neighbors(u))))))
     for v in list(set(p) - set(g.neighbors(u))):
-        # print("v: {}".format(v))
-        # print("r = {} p = {} x = {}".format(r + [v], list(set(p) & set(g.neighbors(v))), list(set(x) & set(g.neighbors(v)))))
-        bronKerbosh(cliqueMax, g, r + [v], list(set(p) & set(g.neighbors(v))), list(set(x) & set(g.neighbors(v))))
-        # print("p = {} - {}".format(list(set(p)), list(set(g.neighbors(v)))))
+        bronKerbosh(n, cliqueMax, g, r + [v], list(set(p) & set(g.neighbors(v))), list(set(x) & set(g.neighbors(v))))
         p.remove(v)
         x.append(v)
-        # print("p = {} x = {}".format(p, x))
-        # print("fimbk")
 
 def escolheVertice(g, vertices):
     grau = []
--- main.py
+++ main.py
@@ -1,33 +1,84 @@
-from igraph import*
+# -*- coding: utf-8 -*-
+from igraph import *
 from bronKerbosh import bronKerbosh
 import sys
+import time
 
-n = int(sys.argv[1])
+start_time = time.time()
+
+if len(sys.argv) <= 1:
+    print("Não foi inserido o argumento referente ao tamanho de n.\nValor default n = 8.")
+    n = 8
+else:
+    n = int(sys.argv[1])
+
 g = Graph()
 g.add_vertices(n*n)
-# for i in range(n*n):
-#     g.add_vertex(i)
-# for i in range(n*n):
-#     for j in range(n*n):
-#         if ((abs(j-i))%n == abs(i-j)/9 and abs(i-j) <= 9 and i!=j)or (abs(j-i))%n == 0 and i!=j:
-#             g.add_edge(g.vs[i].index,g.vs[j].index)
-#         elif abs(i-j) <= n and (i+(i%n))%n == (j+(j%n))%n and i!= j:
-#             g.add_edge(g.vs[i].index,g.vs[j].index)
-# plot(g)
-# print(str(len(g.neighbors(g.vs[0].index))))
 
-# g.complementer().simplify()
-
-for i in range(n*n):
+for i in range(n*n):                    # Adicionando arestas relacionadas aos movimentos das rainhas nas n*n posições do tabuleiro
     for j in range(n*n):
-        if i != j and ((j-i) % n == 0 or (j-i) % (n+1) == 0 or (j-i) % (n-1) == 0 or i//n == j//n):
+        if i != j and ((j-i) % n == 0 or ((j-i) % (n+1) == 0 and abs(i//n - j//n)*(n+1)+i == j) or ((j-i) % (n-1) == 0 and abs(i//n - j//n)*(n-1)+i == j) or i//n == j//n):
             g.add_edge(g.vs[i].index, g.vs[j].index)
 
-# plot(g)
-for v in range(n*n):
-    print(len(g.simplify().neighbors(v)))
-# plot(g.complementer().simplify())
-cliqueMax = []
-bronKerbosh(cliqueMax, g.complementer().simplify(), list(), list(g.vs.indices), list())
-# print(cliqueMax)
-print(len(cliqueMax))
+cliqueMax = []                  # Executa o BK no grafo complementar
+bronKerbosh(n, cliqueMax, g.simplify().complementer().simplify(), list(), list(g.vs.indices), list())
+
+print("\n- Tempo gasto para modelar o grafo e aplicar o BronKerbosh %ss  -" % (time.time() - start_time))
+print("\n{} soluções distintas para o problema das {} rainhas foram encontradas.".format(len(cliqueMax), n))
+
+tab = [[0 for x in range(n)] for y in range(n)]
+
+while True:
+
+    op = int(input('\n[0] Fechar programa\n[1] Listar todas as soluções encontradas\n>>> '))
+
+    if op == 1:
+        for clique in cliqueMax:
+            print("{}. {}".format(cliqueMax.index(clique), clique))
+
+        op = int(input("\nDeseja ver algumas destas soluções no tabuleiro?\n[0] Não, fechar programa \n[1] Sim\n>>> "))
+
+        if op == 0:
+            print("\nPrograma finalizado.")
+            break
+
+        elif op == 1:
+
+            while (True):
+
+                op = int(input("\nQual solução? (0 a {}) [Digite -1 para sair]\n>>> ".format(len(cliqueMax)-1)))
+
+                if op == -1:
+                    break
+
+                elif op >= 0 and op < len(cliqueMax):
+                    for i in range(n):
+                        for j in range(n):
+                            if j+i*n in cliqueMax[op]:
+                                tab[i][j] = '♛'
+                            else:
+                                tab[i][j] = '○'
+                    print("")
+
+                    tab[::-1]
+                    for i in range(n):
+                        for j in range(n):
+                            print("{} ".format(tab[i][j])),
+                        print("")
+
+                else:
+                    print("\nDigite uma das opções acima.")
+
+            print("\nPrograma finalizado.")
+            break
+
+        else:
+            print("\nDigite uma das opções acima.")
+
+
+    elif op == 0:
+        print("\nPrograma finalizado.")
+        break
+
+    else:
+        print("\nDigite uma das opções acima.")
